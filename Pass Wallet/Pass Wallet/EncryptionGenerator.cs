2a2d915e552598e33b076a1a73b7c335b9c5a376
Sourashis.Bhowmik <sourashis.bhowmik@fdmgroup.com>
2024-01-23 16:14:49 +0800

    Pass Wallet initial commit

---
+++ Pass Wallet/Pass Wallet/EncryptionGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Security.Cryptography;
+
+
+namespace Pass_Wallet
+{
+    public static class EncryptionKeyGenerator
+    {
+        public static byte[] GenerateKey(int keySize = 32) // Default to 256 bits (32 bytes)
+        {
+            using var rng = new RNGCryptoServiceProvider();
+            byte[] key = new byte[keySize];
+            rng.GetBytes(key);
+            return key;
+        }
+    }
+}
